fa75b9536e6b264d27dedd3c3b49dcc19632f23e
arcnirwal <154900947+arcnirwal@users.noreply.github.com>
2024-04-08 10:01:56 +0530

    Add files via upload

---
+++ fas.js
@@ -0,0 +1,126 @@
+
+
+//  variable
+// const obj ={
+//     "name": "ajay",
+//     "lastname":"singh"
+// }
+
+// // const arr=(1,2,34);
+// // console.log(Array.isArray(obj))
+// // console.log(Array.isArray(arr))
+// for( let i=0; i<2; i++)
+//  for(let i of arr){
+// console.log(i);
+//  }
+// for (let i in obj){
+//     console.log(i);
+// }
+// console.log(window)
+// console.log(window.innerHeight)
+// console.log(window.innerWidth)
+//  console.dir(window.document)
+//  console.log(window)  
+//   console.dir(window) 
+//  console.log(
+//     console
+//  )
+// const res =document.getElementsByTagName("h2")
+// console.log(res)
+// const res =document.getElementsByTagName("h2")
+//  console.log(res)
+// console.log(document.getElementById("try"))
+// const res= document.querySelector("#try");
+// res.style.backgroundColor = "red"
+
+
+// const ne = document.createElement("h1")
+// console.log(ne);
+// ne.innerText="dynamic text";
+// document.body.appendChild(ne);
+// const rem= document.getElementsByTagName("h1")
+// console.log(rem);
+//  document.body.removeChild(rem[1])
+// function getinfo(){
+//     const d= document.querySelector("div");
+//     d.append("hello world")
+//     console.log("clicked")
+//
+
+//  function getInfo(e){
+//     console.log(e);
+//     let a=document .querySelecto("button");
+//     e.target.style.backgroundcolor="green";
+//     e.target.style.color="red"
+//  }
+// const firstnamechange= ()=>{
+// console.log('first name');
+
+// }
+// let fun1 = (e)=>{
+//     console.log(e.target.value);
+//     if(e.target.value.length<5){
+//         e.target.style.border="2px solid green"
+//     }
+//         else{
+// e.target.style.border="2px solid green";
+// }
+// let submitForm = (e) => {
+//     e.preventDefault();
+//     let form = document.forms[0];
+//     let data = new FormData(form);
+//     let obj = {};
+//     for (let [key, value] of data.entries()) {
+//         obj[key] = value;
+//     }
+//     console.log(obj);
+// }
+// function submitForm(e){
+//     e.prevenrdefault();
+//     constt=e.target
+//     for(leti=0;i<this.length;i++){
+//         const ty=t[i].type;
+//         const vt=t[i].value
+//         console.log(t(i).value);
+//         console.log(t(i).type);
+//         if(ty=='checkbox'){
+//             console.log(t(i).checkbox)
+//         }
+//     }
+// }
+// function submitForm(e){
+//     e.preventdefault();
+//     const=e.target;
+//     const res={
+//         hobbies 11,
+//         i=0;i<1.length; i++)
+//         {
+//             const ty=target(i).type;
+//             const vt=t(i)
+//         }
+// function submitform(e) {
+//     e.preventdefault();
+//     const t = e.target;
+//     cosntt = {
+//         hobbies: {},
+//     };
+//     for (let i = 0; i < t.length; j++) {
+//         const ty = t(i).type;
+//         if (ty == "chechkbox" && t(i), checked) {
+//             resizeBy.hobbies.push(v1);
+//         }
+//         if (ty != "checkbox") {
+//             res(nm) = vl;
+//         }
+//     }
+// }
+function sum(a,b,fun){
+    return a+b;
+}
+function print(x){
+    console.log('   ***',x ,'     ')
+}
+sum(3,4,print);
+
+
+
---
+++ object.js
@@ -0,0 +1,9 @@
+// Sample object
+let obj = { key: 'value' };
+
+// Check if the object is an array
+if (Array.isArray(obj)) {
+    console.log("Object is an array.");
+} else {
+    console.log("Object is not an array.");
+}
